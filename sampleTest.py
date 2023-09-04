44a4f511d08c92678e864005081c6d0daa40c42f
CristianS <csalvatierra007@gmail.com>
2023-09-04 05:35:55 -0400

    1st version of Samples Codes

--- sampleTest.py
+++ sampleTest.py
@@ -8,3 +8,25 @@ s=a('data science','Curso completo')
 t=a._make(['inteligencia artificial','conceptos basicos'])
 print(t)
 print(s)
+
+i = 0
+while i<9:
+    print("Number:",i)
+    i+=1
+print("Bye")
+
+import random
+to_be_guessed = int(20*random.random())+1
+guess = 0
+while guess != to_be_guessed:
+    guess = int(input("New number: "))
+    if guess > 0:
+        if guess > to_be_guessed:
+            print("Number too large")
+        elif guess < to_be_guessed:
+            print("Number too small")
+    else:
+        print("Sorry that you are giving up!")
+        break
+else:
+    print("Congratulation! You made it!")
